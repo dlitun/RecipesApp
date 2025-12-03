aaf979ba0c3d40fc4b9dc8a423483edc060dda5c
dlitun <dlitun@ya.ru>
2025-12-03 17:39:19 +0300

    RA-03: change nav-buttons

--- app/src/main/java/com/example/recipesapp/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/MainActivity.kt
@@ -7,16 +7,26 @@ import com.example.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupSystemBars()
+    }
+
+    private fun setupSystemBars() {
         val bgColor = ContextCompat.getColor(this, R.color.color_main_background)
         window.statusBarColor = bgColor
         window.navigationBarColor = bgColor
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }

a2034e324f244a33ad868f7cb2c2dbc056dc5488
dlitun <dlitun@ya.ru>
2025-12-19 15:24:37 +0300

    RA-09: categories-click-callback

---
+++ app/src/main/java/com/example/recipesapp/RecipesListFragment.kt
@@ -0,0 +1,28 @@
+package com.example.recipesapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.recipesapp.databinding.FragmentRecipesListBinding
+
+class RecipesListFragment : Fragment() {
+
+    private var _binding: FragmentRecipesListBinding? = null
+    private val binding get() = _binding ?: error("FragmentRecipesListBinding is null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecipesListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
